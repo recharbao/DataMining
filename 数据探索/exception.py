6bd9da42534a3e1f992b922372469c6146f6b9a1
rechar <884520248@qq.com>
2020-10-09 20:00:56 +0800

    初始数据

---
+++ 数据探索/exception.py
@@ -0,0 +1,36 @@
+#-*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+import pandas as pd
+
+catering_sale = '/media/rechar/新加卷/github仓库/数据挖掘/data/chapter3/demo/data/catering_sale.xls' 
+data = pd.read_excel(catering_sale, index_col = u'日期') 
+
+print(data.describe())
+print(len(data))
+
+
+import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+plt.figure() 
+p = data.boxplot(return_type='dict')
+x = p['fliers'][0].get_xdata()
+y = p['fliers'][0].get_ydata()
+y.sort() 
+
+
+for i in range(len(x)): 
+  if i>0:
+    plt.annotate(y[i], xy = (x[i],y[i]), xytext=(x[i]+0.05 -0.8/(y[i]-y[i-1]),y[i]))
+  else:
+    plt.annotate(y[i], xy = (x[i],y[i]), xytext=(x[i]+0.08,y[i]))
+
+plt.show() 
+
+#figure1
+
+
