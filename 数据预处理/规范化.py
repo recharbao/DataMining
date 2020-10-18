0e91e58094545cd859a0e4aac79deadb250ce2e4
rechar <884520248@qq.com>
2020-10-19 01:13:27 +0800

    规范化

--- 数据预处理/规范化.py
+++ 数据预处理/规范化.py
@@ -1,8 +1,15 @@
 #-*- coding: utf-8 -*-
 
 import pandas as pd
+import numpy as np
 
-datafile = ''
+datafile = '/media/rechar/新加卷/github仓库/DataMining/数据预处理/data/normalization_data.xls'
 
+data = pd.read_excel(datafile, header=None)
 
+print((data - data.min())/(data.max() - data.min()))
+
+print((data - data.mean())/data.std())
+
+print((data/10 ** np.ceil(np.log10(data.abs().max()))))
 
